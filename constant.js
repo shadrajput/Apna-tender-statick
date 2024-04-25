74daad083f231e9f8ae0305d0b4d11d2f11b9bad
shadrajput <shadrajput6@gmail.com>
2024-04-25 16:58:51 +0530

    Build Project

---
+++ constant.js
@@ -0,0 +1,10 @@
+export const GOOGLE_CLIENT_ID =
+  "499491102377-tppkd0posonhog3ft4e6b1arv40or7eh.apps.googleusercontent.com";
+
+export const API_BASE_URL = "http://localhost:4000";
+
+export const RECAPTCHA_SITE_KEY = "6LcRswgpAAAAABZxocKi4YpPtZOIMOxMNpBsrzTc";
+
+export const SMTP_SECURE_TOKEN = "7634fac9-51d7-4443-831c-5304c1c8362d";
+
+export const EMAIL_TO = "karadiyasadikali786@gmail.com";
