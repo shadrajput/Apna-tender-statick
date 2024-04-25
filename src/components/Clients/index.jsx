00657aea7b0e7ed153633816d113bcae06531f99
shadrajput <shadrajput6@gmail.com>
2024-04-25 21:53:02 +0530

    Header and Hero Responsive

--- src/components/Clients/index.jsx
+++ src/components/Clients/index.jsx
@@ -14,60 +14,60 @@ const Clients = () => {
   };
 
   return (
-    <section className="pt-20">
+    <section className="pt-7 xl:pt-20 pb-5">
       <h1 className="text-center text-slate-400">Collaborating with 3200+internationally renowned companies.</h1>
       <div className="relative">
-        <div className="blur-lg absolute top-0 bg-white left-0 z-50  h-full w-40"></div>
-        <div className="py-10 overflow-hidden mx-20 ">
+        <div className="blur-lg absolute top-0  bg-white left-0 z-50  h-full w-12 sm:w-20 xl:w-40"></div>
+        <div className="py-10 overflow-hidden mx-3 sm:mx-6 xl:mx-20 ">
           <Slider {...settings}>
             <div>
-              <img src="/images/brands/New folder/1.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/1.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/2.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/2.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/3.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/3.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/4.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/4.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/5.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/5.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/6.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/6.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/7.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/7.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/8.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/8.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/9.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/9.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/10.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/10.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/11.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/11.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/12.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/12.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/13.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/13.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/14.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/14.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
             <div>
-              <img src="/images/brands/New folder/15.png" alt="" className="w-[160px]" />
+              <img src="/images/brands/New folder/15.png" alt="" className="w-full md:w-[100px] xl:w-[160px]" />
             </div>
           </Slider>
         </div>
-        <div className="blur-lg absolute top-0 bg-white right-0 z-50 r h-full w-40"></div>
+        <div className="blur-lg absolute top-0 bg-white right-0  z-50 h-full w-12 sm:w-20 xl:w-40"></div>
       </div>
     </section>
   );
